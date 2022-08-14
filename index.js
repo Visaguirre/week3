25decf0cc8c4339dbe0e4c99ec346d4331a125ed
Visaguirre <vinnyisaguirre@gmail.com>
2022-08-14 10:59:07 -0600

    week3

---
+++ index.js
@@ -0,0 +1,16 @@
+let itemPrice = 1.00;
+let moneyInWallet = 20;
+let numberOfFriends = 3;
+let myAge = 33;
+const firstName = "Johhny";
+const lastName = "Good";
+const middleInitial = "B"
+
+let currentWalletAmount = (moneyInWallet - itemPrice);
+    console.log ( "My networth is " + currentWalletAmount + " dollars. ");
+
+ let friendGrowth = (numberOfFriends / myAge)
+    console.log("Whoo Ho, I made " + friendGrowth + " friends this year!"); //this is generous
+
+let fullName = (firstName + " " + middleInitial +" "+ lastName)
+    console.log(fullName);
